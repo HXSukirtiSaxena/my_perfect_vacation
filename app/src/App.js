61bd50be2c05a3ae10b3d20c20438daa0ea18d4f
Sukirti Saxena <sukirti.saxena@H3298.local>
2022-05-05 16:12:19 +0100

    remove the enter button

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,24 @@
 import image from './image.png';
 import { useState } from 'react'
-
-import './App.css';
 import QuestionGroup1 from "./QuestionGroup1.js";
+import './App.css';
 
 function App() {
-
   const [ isAboutVisible, setIsAboutVisible] = useState(false)
-  const [show,setShow]=useState(true)
-
+  const [ isShown, setIsShown] = useState(true)
+  const handleClick = () => {
+    setIsShown(false)
+    setIsAboutVisible(true)
+  }
   return (
-    <div className="App">
 
-      <img src={image} alt="logo" />
-        <p>
-        Where to go in vacation?
-        </p>
-          <button id= 'btn' onClick={()=> setIsAboutVisible(true)}> Enter Here  </button> 
-          { isAboutVisible ?  <QuestionGroup1/> : null }
+      <div className="App">
+        <img src={image} alt="logo" />
+          <h2> Let us help you decide where to go on vacation? </h2>
+            {isShown ? <button id= 'btn' onClick={()=> handleClick()}> Enter Here  </button> : null}
+            { isAboutVisible ?  <QuestionGroup1/> : null }
+      </div>
 
-    </div>
   );
 }
 export default App;
-
-
-
-
-
-
-
