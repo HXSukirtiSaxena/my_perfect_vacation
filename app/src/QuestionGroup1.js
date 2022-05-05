a8ad5f61bd0dc7d976d8b7f272f5e7d3d613a204
Sukirti Saxena <sukirti.saxena@H3298.local>
2022-05-05 12:24:18 +0100

    first page

---
+++ app/src/QuestionGroup1.js
@@ -0,0 +1,9 @@
+const questions = [
+    {
+      questionText: 'Where do you want to go for vacations?',
+      answerOptions: [
+        { answerText: 'Local', isCorrect: false },
+        { answerText: 'International', isCorrect: false },
+      ],
+    },
+  ]
